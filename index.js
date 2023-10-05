cd14dd5d49dba048e210a0282cc643da61c70d3e
Abaadir <abaadir.yasin@student.moringaschool.com>
2023-10-05 03:47:57 +0300

    completed

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+
+const multiply = num1 * num2;
+
+const random = Math.floor(Math.random() * 100) + 1;
+
+
+const num3 = 18;
+const num4 = 14;
+const mod = num3 % num4;
+
+const numberSet = [8, 10, 20, 12, 9];
+const max = Math.max(...numberSet);
